fe4807a9b92ba6e42640d42fccb8c9ec2636d123
shiwanming <1633138551@qq.com>
2018-05-02 21:26:13 +0800

    例题4-4信息解码

---
+++ Liurujia/src/unit4/Four4_4.java
@@ -0,0 +1,71 @@
+package unit4;
+
+import java.util.Arrays;
+
+/*
+ *信息解码
+ *编码头$#** /
+ *编码文本0100000101101100011100101000
+ *我的想法就是首先把编码头映射为那个01串
+ *我思考了一下，遍历那个编码文本不需要用双重循环，我只需要用一个变量，然后根据情况的不同来改变它
+ *1取这个数字后面三位的二进制的换算后的值，表示我接下来要循环的次数，
+ *2当我的循环的那个数字正好等于我之前的那个编码长度的数字的时候，我就要重新取后面的三个数字
+ *3循环没有限制，唯一的限制就是这个变量到底了
+ */
+public class Four4_4 {
+	static String[] ys;
+	//判断t的二进制是否都是1
+	public static boolean isOne(int t){
+		if(((t+1)^t)==t+t+1){
+			return true;
+		}
+		return false;
+	}
+	//用来映射01串
+	public static void mapping(){
+		int i = 0,b = 0,t = 0;//b表示的是二进制的实际大小，t表示的是从0开始增长到b
+		while(i<ys.length){//这个for是用来给ys赋值的，当循环结束，赋值完毕
+			String s = Integer.toBinaryString(b);
+			if(isOne(b)&&t!=b){
+				ys[i++] = String.format("%0"+s.length()+"d", Integer.parseInt(Integer.toBinaryString(t++)));
+			}else{
+				t=0;
+				b++;
+			}
+		}
+	}
+	//查找下标的方法
+	public static int getValue(String s){
+		for(int i = 0;i<ys.length;i++){
+			if(s.equals(ys[i])){
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static void main(String[] args) {
+		String bHead = "$#**/";
+		String bCode = "0100000101101100011100101000";
+//		System.out.println(String.format("%04d", Integer.parseInt(Integer.toBinaryString(0))));
+		//我要将编码头做一个映射
+		ys = new String[bHead.length()];
+		mapping();
+		System.out.println(Arrays.toString(ys));
+		int i = 0;//主要的一个变量
+		while(i<bCode.length()){
+
+			String s = bCode.substring(i, i+3);
+			i+=3;
+			int n = Integer.parseInt(s,2);//2进制转成10进制
+			System.out.println(n);
+			String sb = bCode.substring(i,i+n);
+			while(!isOne(Integer.parseInt(sb,2))){
+				System.out.println(i+"**********************"+i+n);
+				sb = bCode.substring(i,i+n);
+				System.out.println(sb);
+//				System.out.println(bHead.charAt(getValue(sb))+" "+sb);
+				i+=n;
+			}
+		}
+	}
+}
