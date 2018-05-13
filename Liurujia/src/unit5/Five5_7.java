88f3283bbb1397dc7d19f3eda6f2fd7d376c4cb3
shiwanming <1633138551@qq.com>
2018-05-13 21:55:03 +0800

    centos 上传的丑数

--- Liurujia/src/unit5/Five5_7.java
+++ Liurujia/src/unit5/Five5_7.java
@@ -1,35 +1,42 @@
 package unit5;
 
+import java.io.BufferedOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 
 /*
  * ³óÊý
  */
 public class Five5_7 {
 	public static void main(String[] args) {
-		int n = 2;
-		int count = 1; 
+		int count = 5;
+		List<Integer> list = new ArrayList<Integer>();
+		int n = 6;
 		while(true){
-			int flag = 0;
-			for(int i = 2;i<=n;i++){
-				int j = 2;
-				for(;j<i;j++){
-					if(i%j==0){
+			if(n%2==0||n%3==0||n%5==0){
+				boolean flag = true;
+				for(Integer i : list){
+					if(n%i==0){
+						flag = false;
 						break;
 					}
+//					System.out.println(i);
 				}
-				if(j==i&&j%2!=0&&j%3!=0&&j%5!=0){
-					if(n%j==0){
-						flag = 1;
-						break;
-					}
+				if(flag){
+					count++;
+					System.out.println(n+"choushu");
 				}
+			}else{
+				list.add(n);
 			}
-			if(flag == 0){
-				count++;
-				System.out.println(n);
-			}
+			/*PrintStream out = new PrintStream(new BufferedOutputStream(System.out));
+			out.println(count+"-----");*/
+			System.out.println(count+"----");
 			if(count == 1500){
-				System.out.println(n);
+				System.out.println(n+"***");
 				break;
 			}
 			n++;
